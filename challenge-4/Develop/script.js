6f7586121b5fedeaa2fdaa99448384bfc18db709
konawave <nickkjeffs@gmail.com>
2023-05-01 12:59:41 -0400

    added basic variables in JS

---
+++ challenge-4/Develop/script.js
@@ -0,0 +1,11 @@
+var highscoreBtn = document.querySelector("#highscore")
+var timer = document.querySelector("#timer")
+
+// Variables for answer buttons
+var answer1 = document.querySelector("#answer1")
+var answer2 = document.querySelector("#answer2")
+var answer3 = document.querySelector("#answer3")
+var answer4 = document.querySelector("#answer4")
+
+var initials = ""
+var timerCount = 80
