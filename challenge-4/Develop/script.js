30be5e07bb3fd45de931e05219848df98a9a1440
konawave <nickkjeffs@gmail.com>
2023-05-01 17:10:16 -0400

    further changes to js, including adding a timer and event listener. Next step is to figure out how to make the start button disappear using data attributes.

--- challenge-4/Develop/script.js
+++ challenge-4/Develop/script.js
@@ -1,5 +1,7 @@
 var highscoreBtn = document.querySelector("#highscore")
 var timer = document.querySelector("#timer")
+var startBtn = document.querySelector("#start") //No start button quite yet. Will use datasets to make it visible/hidden. 
+var questionBtn = document.querySelector("#question")
 
 // Variables for answer buttons
 var answer1 = document.querySelector("#answer1")
@@ -8,4 +10,33 @@ var answer3 = document.querySelector("#answer3")
 var answer4 = document.querySelector("#answer4")
 
 var initials = ""
-var timerCount = 80
+
+function countdown(){
+    var timerCount = 80
+    var timerCountdown = setInterval(function() {   
+    
+    if (timerCount > 0) {
+        timer.textContent = timerCount;
+        timerCount--;
+    } else {
+        timer.textContent = 0
+        clearInterval(timerCountdown);
+    }
+    } , 1000)
+}
+
+// Event listener for highscoreBtn:
+startBtn.addEventListener("click", function() {
+    countdown();
+    questionBtn.textContent = "When Gandalf was battling the Balrog in Moria, he said: 'Fly _______'";
+    console.log(question);
+    answer1.textContent = "My friends.";
+    answer2.textContent = "You fools.";
+    answer3.textContent = "Away from here.";
+    answer4.textContent = "Far away.";
+
+
+})
+// Event listener for each answer question
+
+// 
